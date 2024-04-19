6a4aa236363aea3479f0cbc60e5a74493864adfb
Jayson Hundsrucker <hundsruckerjayson@gmail.com>
2024-04-18 18:06:43 -0700

    Completed Assignment

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("level", true)]
+        public void IsAPalindrome_Test(string input, bool expected)
         {
+            // Arrange
+            var wordSmith = new PalindromeExerciseTests.WordSmith();
 
+            // Act
+            bool result = wordSmith.IsAPalindrome(input);
+
+            // Assert
+            Assert.Equal(expected, result);
         }
     }
 }
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmith.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            // Convert the word to lowercase to ignore case sensitivity
+            word = word.ToLower();
+
+            // Initialize pointers for the start and end of the word
+            int left = 0;
+            int right = word.Length - 1;
+
+            // Traverse the word from both ends towards the middle
+            while (left < right)
+            {
+                // Skip non-alphanumeric characters
+                if (!char.IsLetterOrDigit(word[left]))
+                {
+                    left++;
+                    continue;
+                }
+                if (!char.IsLetterOrDigit(word[right]))
+                {
+                    right--;
+                    continue;
+                }
+
+                // If characters at left and right pointers don't match, it's not a palindrome
+                if (word[left] != word[right])
+                {
+                    return false;
+                }
+
+                // Move pointers towards the middle
+                left++;
+                right--;
+            }
+
+            // If the loop completes without finding any mismatch, it's a palindrome
+            return true;
+        }
+    }
+}
